01c210b0182bcf9b4e3b65720d99332d08149995
Robin Jung <robin.s.jung@outlook.com>
2024-09-21 18:34:45 +0200

    feat: added chrome 129 + tests

--- profiles/chrome.go
+++ profiles/chrome.go
@@ -184,3 +184,94 @@ var Chrome_124 = ClientProfile{
 		":path",
 	},
 }
+
+var Chrome_129 = ClientProfile{
+	ClientHelloSpec: func() *tls.ClientHelloSpec {
+		return &tls.ClientHelloSpec{
+			CipherSuites: []uint16{
+				tls.GREASE_PLACEHOLDER,
+				tls.TLS_AES_128_GCM_SHA256,
+				tls.TLS_AES_256_GCM_SHA384,
+				tls.TLS_CHACHA20_POLY1305_SHA256,
+				tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+				tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+				tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+				tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+				tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256,
+				tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256,
+				tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
+				tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+				tls.TLS_RSA_WITH_AES_128_GCM_SHA256,
+				tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
+				tls.TLS_RSA_WITH_AES_128_CBC_SHA,
+				tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+			},
+			CompressionMethods: []byte{
+				0x00, // compressionNone
+			},
+			Extensions: tls.ShuffleChromeTLSExtensions([]tls.TLSExtension{
+				&tls.UtlsGREASEExtension{},
+				&tls.SupportedPointsExtension{SupportedPoints: []byte{
+					0x00, // pointFormatUncompressed
+				}},
+				&tls.PSKKeyExchangeModesExtension{Modes: []uint8{
+					tls.PskModeDHE,
+				}},
+				&tls.ExtendedMasterSecretExtension{},
+				&tls.SCTExtension{},
+				&tls.ApplicationSettingsExtension{SupportedProtocols: []string{"h2"}},
+				&tls.SupportedCurvesExtension{Curves: []tls.CurveID{
+					tls.CurveID(tls.GREASE_PLACEHOLDER),
+					tls.X25519Kyber768Draft00,
+					tls.X25519,
+					tls.CurveP256,
+					tls.CurveP384,
+				}},
+				&tls.SignatureAlgorithmsExtension{SupportedSignatureAlgorithms: []tls.SignatureScheme{
+					tls.ECDSAWithP256AndSHA256,
+					tls.PSSWithSHA256,
+					tls.PKCS1WithSHA256,
+					tls.ECDSAWithP384AndSHA384,
+					tls.PSSWithSHA384,
+					tls.PKCS1WithSHA384,
+					tls.PSSWithSHA512,
+					tls.PKCS1WithSHA512,
+				}},
+				&tls.SNIExtension{},
+				&tls.ALPNExtension{AlpnProtocols: []string{"h2", "http/1.1"}},
+				&tls.StatusRequestExtension{},
+				tls.BoringGREASEECH(),
+				&tls.RenegotiationInfoExtension{Renegotiation: tls.RenegotiateNever},
+				&tls.SupportedVersionsExtension{Versions: []uint16{
+					tls.GREASE_PLACEHOLDER,
+					tls.VersionTLS13,
+					tls.VersionTLS12,
+				}},
+				&tls.SessionTicketExtension{},
+				&tls.UtlsCompressCertExtension{Algorithms: []tls.CertCompressionAlgo{
+					tls.CertCompressionBrotli,
+				}},
+				&tls.KeyShareExtension{KeyShares: []tls.KeyShare{
+					{Group: tls.CurveID(tls.GREASE_PLACEHOLDER), Data: []byte{0}},
+					{Group: tls.X25519Kyber768Draft00},
+					{Group: tls.X25519},
+				}},
+				&tls.UtlsGREASEExtension{},
+				&tls.UtlsPreSharedKeyExtension{OmitEmptyPsk: true},
+			}),
+		}
+	},
+	Settings: []http2.Setting{
+		{ID: http2.SettingHeaderTableSize, Val: 65536},
+		{ID: http2.SettingEnablePush, Val: 0},
+		{ID: http2.SettingInitialWindowSize, Val: 6291456},
+		{ID: http2.SettingMaxHeaderListSize, Val: 262144},
+	},
+	ConnectionFlow: 15663105,
+	PseudoHeaderOrder: []string{
+		":method",
+		":authority",
+		":scheme",
+		":path",
+	},
+}
---
+++ tests/chrome_test.go
@@ -0,0 +1,29 @@
+package tests
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/sparkaio/tlsclient"
+	"github.com/sparkaio/tlsclient/profiles"
+)
+
+func TestChrome(t *testing.T) {
+	c := tlsclient.New(profiles.Chrome_124)
+	c.Get("https://tls.peet.ws/api/clean")
+	res, err := c.Get("https://tls.peet.ws/api/clean")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer res.Body.Close()
+	var data PeetsApiCleanData
+	if err := json.NewDecoder(res.Body).Decode(&data); err != nil {
+		t.Fatal(err)
+	}
+	if data.PeetprintHash != "9cb72b909981b498e833d0f5e5929c70" {
+		t.Errorf("Expected peetprint hash 9cb72b909981b498e833d0f5e5929c70, got %s", data.PeetprintHash)
+	}
+	if data.AkamaiHash != "90224459f8bf70b7d0a8797eb916dbc9" {
+		t.Errorf("Expected akamai hash 605a1154008045d7e3cb3c6fb062c0ce, got %s", data.AkamaiHash)
+	}
+}
