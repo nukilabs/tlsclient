b27634cce545d75927b230832adbab6c9dabcdb3
Robin Jung <robin.s.jung@outlook.com>
2025-10-06 13:05:52 +0200

    feat: add Safari26 client profile and corresponding tests

--- profiles/safari.go
+++ profiles/safari.go
@@ -218,3 +218,107 @@ var Safari18 = ClientProfile{
 		":path",
 	},
 }
+
+var Safari26 = ClientProfile{
+	ClientHelloSpec: func() *tls.ClientHelloSpec {
+		return &tls.ClientHelloSpec{
+			CipherSuites: []uint16{
+				tls.GREASE_PLACEHOLDER,
+				tls.TLS_AES_256_GCM_SHA384,
+				tls.TLS_CHACHA20_POLY1305_SHA256,
+				tls.TLS_AES_128_GCM_SHA256,
+				tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+				tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+				tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256,
+				tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+				tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+				tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256,
+				tls.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA,
+				tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA,
+				tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+				tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
+				tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
+				tls.TLS_RSA_WITH_AES_128_GCM_SHA256,
+				tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+				tls.TLS_RSA_WITH_AES_128_CBC_SHA,
+				tls.FAKE_TLS_ECDHE_ECDSA_WITH_3DES_EDE_CBC_SHA,
+				tls.TLS_ECDHE_RSA_WITH_3DES_EDE_CBC_SHA,
+				tls.TLS_RSA_WITH_3DES_EDE_CBC_SHA,
+			},
+			CompressionMethods: []byte{
+				0x00, // compressionNone
+			},
+			Extensions: []tls.TLSExtension{
+				&tls.UtlsGREASEExtension{},
+				&tls.SNIExtension{},
+				&tls.ExtendedMasterSecretExtension{},
+				&tls.RenegotiationInfoExtension{Renegotiation: tls.RenegotiateOnceAsClient},
+				&tls.SupportedCurvesExtension{Curves: []tls.CurveID{
+					tls.GREASE_PLACEHOLDER,
+					tls.X25519MLKEM768,
+					tls.X25519,
+					tls.CurveP256,
+					tls.CurveP384,
+					tls.CurveP521,
+				}},
+				&tls.SupportedPointsExtension{SupportedPoints: []byte{
+					0x00, // pointFormatUncompressed
+				}},
+				&tls.ALPNExtension{AlpnProtocols: []string{"h2", "http/1.1"}},
+				&tls.StatusRequestExtension{},
+				&tls.SignatureAlgorithmsExtension{SupportedSignatureAlgorithms: []tls.SignatureScheme{
+					tls.ECDSAWithP256AndSHA256,
+					tls.PSSWithSHA256,
+					tls.PKCS1WithSHA256,
+					tls.ECDSAWithP384AndSHA384,
+					tls.PSSWithSHA384,
+					tls.PSSWithSHA384,
+					tls.PKCS1WithSHA384,
+					tls.PSSWithSHA512,
+					tls.PKCS1WithSHA512,
+					tls.PKCS1WithSHA1,
+				}},
+				&tls.SCTExtension{},
+				&tls.KeyShareExtension{KeyShares: []tls.KeyShare{
+					{Group: tls.CurveID(tls.GREASE_PLACEHOLDER), Data: []byte{0}},
+					{Group: tls.X25519MLKEM768},
+					{Group: tls.X25519},
+				}},
+				&tls.PSKKeyExchangeModesExtension{Modes: []uint8{
+					tls.PskModeDHE,
+				}},
+				&tls.SupportedVersionsExtension{Versions: []uint16{
+					tls.GREASE_PLACEHOLDER,
+					tls.VersionTLS13,
+					tls.VersionTLS12,
+				}},
+				&tls.UtlsCompressCertExtension{Algorithms: []tls.CertCompressionAlgo{
+					tls.CertCompressionZlib,
+				}},
+				&tls.UtlsGREASEExtension{},
+			},
+		}
+	},
+	H2: &H2ClientProfile{
+		Settings: []http2.Setting{
+			{ID: http2.SettingEnablePush, Val: 0},
+			{ID: http2.SettingMaxConcurrentStreams, Val: 100},
+			{ID: http2.SettingInitialWindowSize, Val: 2097152},
+			{ID: http2.SettingNoRFC7540Priorities, Val: 1},
+		},
+		ConnectionFlow: 10420225,
+	},
+	H3: &H3ClientProfile{
+		Settings: []http3.Setting{
+			{ID: http3.SettingQpackMaxTableCapacity, Val: 16383},
+			{ID: http3.SettingQpackBlockedStreams, Val: 100},
+			{ID: http3.SettingGrease, Val: 0},
+		},
+	},
+	PseudoHeaderOrder: []string{
+		":method",
+		":scheme",
+		":authority",
+		":path",
+	},
+}
--- tests/safari_test.go
+++ tests/safari_test.go
@@ -26,3 +26,22 @@ func TestSafari18(t *testing.T) {
 		t.Errorf("Expected akamai hash c52879e43202aeb92740be6e8c86ea96, got %s", data.AkamaiHash)
 	}
 }
+
+func TestSafari26(t *testing.T) {
+	c := tlsclient.New(profiles.Safari26)
+	res, err := c.Get("https://tls.peet.ws/api/clean")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer res.Body.Close()
+	var data PeetsApiCleanData
+	if err := json.NewDecoder(res.Body).Decode(&data); err != nil {
+		t.Fatal(err)
+	}
+	if data.PeetprintHash != "62b834de729e78a9f0ebd1dd099314a7" {
+		t.Errorf("Expected peetprint hash 62b834de729e78a9f0ebd1dd099314a7, got %s", data.PeetprintHash)
+	}
+	if data.AkamaiHash != "c52879e43202aeb92740be6e8c86ea96" {
+		t.Errorf("Expected akamai hash c52879e43202aeb92740be6e8c86ea96, got %s", data.AkamaiHash)
+	}
+}
