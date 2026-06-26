7faab892dc93fd16bf484285451e42f084ed1afc
Robin Jung <robin.s.jung@outlook.com>
2026-06-26 15:51:24 +0200

    feat: chrome 146-149

--- profiles/chrome.go
+++ profiles/chrome.go
@@ -84,6 +84,14 @@ func Chrome(major int) ClientProfile {
 		return Chrome144
 	case 145:
 		return Chrome145
+	case 146:
+		return Chrome146
+	case 147:
+		return Chrome147
+	case 148:
+		return Chrome148
+	case 149:
+		return Chrome149
 	default:
 		return Chrome133
 	}
@@ -546,3 +554,122 @@ var Chrome143 = Chrome133
 var Chrome144 = Chrome133
 
 var Chrome145 = Chrome133
+
+var Chrome146 = ClientProfile{
+	ClientHelloSpec: func() *tls.ClientHelloSpec {
+		return &tls.ClientHelloSpec{
+			CipherSuites: []uint16{
+				tls.GREASE_PLACEHOLDER,
+				tls.TLS_AES_128_GCM_SHA256,
+				tls.TLS_AES_256_GCM_SHA384,
+				tls.TLS_CHACHA20_POLY1305_SHA256,
+				tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+				tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+				tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+				tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+				tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256,
+				tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256,
+				tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
+				tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+				tls.TLS_RSA_WITH_AES_128_GCM_SHA256,
+				tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
+				tls.TLS_RSA_WITH_AES_128_CBC_SHA,
+				tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+			},
+			CompressionMethods: []byte{
+				0x00, // compressionNone
+			},
+			Extensions: tls.ShuffleChromeTLSExtensions([]tls.TLSExtension{
+				&tls.UtlsGREASEExtension{},
+				&tls.RenegotiationInfoExtension{Renegotiation: tls.RenegotiateNever},
+				&tls.PSKKeyExchangeModesExtension{Modes: []uint8{
+					tls.PskModeDHE,
+				}},
+				&tls.SessionTicketExtension{},
+				&tls.SupportedPointsExtension{SupportedPoints: []byte{0x00}},
+				&tls.SignatureAlgorithmsExtension{SupportedSignatureAlgorithms: []tls.SignatureScheme{
+					tls.ECDSAWithP256AndSHA256,
+					tls.PSSWithSHA256,
+					tls.PKCS1WithSHA256,
+					tls.ECDSAWithP384AndSHA384,
+					tls.PSSWithSHA384,
+					tls.PKCS1WithSHA384,
+					tls.PSSWithSHA512,
+					tls.PKCS1WithSHA512,
+				}},
+				&tls.SupportedCurvesExtension{Curves: []tls.CurveID{
+					tls.CurveID(tls.GREASE_PLACEHOLDER),
+					tls.X25519MLKEM768,
+					tls.X25519,
+					tls.CurveP256,
+					tls.CurveP384,
+				}},
+				&tls.SNIExtension{},
+				tls.BoringGREASEECH(),
+				&tls.KeyShareExtension{KeyShares: []tls.KeyShare{
+					{Group: tls.CurveID(tls.GREASE_PLACEHOLDER), Data: []byte{0}},
+					{Group: tls.X25519MLKEM768},
+					{Group: tls.X25519},
+				}},
+				&tls.UtlsCompressCertExtension{Algorithms: []tls.CertCompressionAlgo{
+					tls.CertCompressionBrotli,
+				}},
+				&tls.ApplicationSettingsExtensionNew{
+					SupportedProtocols: []string{"h2"},
+				},
+				&tls.ALPNExtension{AlpnProtocols: []string{"h2", "http/1.1"}},
+				&tls.StatusRequestExtension{},
+				&tls.SupportedVersionsExtension{Versions: []uint16{
+					tls.GREASE_PLACEHOLDER,
+					tls.VersionTLS13,
+					tls.VersionTLS12,
+				}},
+				&tls.SCTExtension{},
+				&tls.ExtendedMasterSecretExtension{},
+				&tls.GenericExtension{Id: 0xca34, Data: []byte{0x00, 0x00}},
+				&tls.UtlsGREASEExtension{},
+				&tls.UtlsPreSharedKeyExtension{OmitEmptyPsk: true},
+			}),
+		}
+	},
+	H2: &H2ClientProfile{
+		Settings: []http2.Setting{
+			{ID: http2.SettingHeaderTableSize, Val: 65536},
+			{ID: http2.SettingEnablePush, Val: 0},
+			{ID: http2.SettingInitialWindowSize, Val: 6291456},
+			{ID: http2.SettingMaxHeaderListSize, Val: 262144},
+		},
+		ConnectionFlow: 15663105,
+		HeaderPriority: func(r *http.Request) http2.PriorityParam {
+			return http2.PriorityParam{
+				StreamDep: 0,
+				Exclusive: true,
+				Weight:    chromeweight(r.Priority),
+			}
+		},
+		InflowTimeout:   5 * time.Second,
+		ReadIdleTimeout: 10 * time.Second,
+		PrefacePing:     true,
+	},
+	H3: &H3ClientProfile{
+		Settings: []http3.Setting{
+			{ID: http3.SettingQpackMaxTableCapacity, Val: 65536},
+			{ID: http3.SettingMaxFieldSectionSize, Val: 262144},
+			{ID: http3.SettingQpackBlockedStreams, Val: 100},
+			{ID: http3.SettingH3Datagram, Val: 1},
+			{ID: http3.SettingGrease, Val: 0},
+		},
+	},
+	PseudoHeaderOrder: []string{
+		":method",
+		":authority",
+		":scheme",
+		":path",
+	},
+}
+
+var Chrome147 = Chrome146
+
+var Chrome148 = Chrome146
+
+var Chrome149 = Chrome146
